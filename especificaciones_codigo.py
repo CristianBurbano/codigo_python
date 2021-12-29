e2bd7835cca9b4bd490f4b6150e372b323cffbdd
Cristian Burbano <cristian.burbano@origen.com.co>
2021-12-29 17:00:29 -0500

    Unit_Test

---
+++ especificaciones_codigo.py
@@ -0,0 +1,8 @@
+def suma(a, b):
+    """
+        suba a y b
+        a int este es un un numero sabroso
+        b int es otro numero no sea mamón
+        returns nada por que es vacio
+    """
+    return a+b
